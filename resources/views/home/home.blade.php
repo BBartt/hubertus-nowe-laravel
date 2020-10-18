85a48586d875894f298afa581a8ae63e0977462a
bartek <bartek2@onet.eu>
2020-10-18 21:51:46 +0200

    setup main page and add images

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="index-section">
+  <div>
+    <img src="{{ asset('images/home/zdjecie czlonkow kola hubertus.jpg') }}" alt="zdjecie czlonkow kola" />
+    <br />
+    <em class="oath">
+      "Przystępując do grona polskich myśliwych ślubuję uroczyście przestrzegać
+      sumiennie praw łowieckich, postępować zgodnie z zasadami etyki łowieckiej,
+      zachowywać tradycje polskiego łowiectwa, chronić przyrodę ojczystą, dbać o
+      dobre imię łowiectwa i godność polskiego myśliwego."
+    </em>
+  </div>
+
+  <section class="club-info">
+    <h1 class="h1">Witamy na stronie internetowej Koła Łowieckiego "HUBERTUS" w Nowem</h1>
+    <p class="paragraph">
+      W dniu 9 lipca 1949r Koło przyjęło nazwę „Hubertus”, a w roku 1953
+      nastąpiło połączenie kół łowieckich z Nowego i Warlubia, będącego
+      pierwszym od 1945r prawnie zorganizowanym Kołem Łowieckim w powiecie
+      świeckim. W wyniku tego połączenia, zwiększyła się powierzchnia terenów
+      łowieckich, ponieważ doszły znaczne obszary leśne.
+    </p>
+    <p class="paragraph">
+      Działalność członków naszego Koła została zauważona przez władze Polskiego
+      Związku Łowieckiego i za osiągnięcia oraz włożony trud pracy dla dobra
+      przyrody ojczystej Koło nasze otrzymało „Honorowy Żeton Zasługi”
+      (określany jako „Złom") – jest to najwyższe odznaczenie łowieckie
+      ustanowione 28 listopada 1929 roku uchwałą Wydziału Wykonawczego Polskiego
+      Związku Stowarzyszeń Łowieckich.
+    </p>
+  </section>
+</section>
+@endsection
